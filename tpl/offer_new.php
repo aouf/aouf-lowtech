3de989f426b3793f6c84d08040f3c6796ba36411
Gregory Colpart <gcolpart+git@evolix.fr>
2019-04-21 00:56:59 +0200

    Correction bug

--- tpl/offer_new.php
+++ tpl/offer_new.php
@@ -19,28 +19,28 @@ if (isset($_POST['titre'])) {
 $uri = $_SERVER['REQUEST_URI'];
 $description = "Décrivez ce que vous proposez.";
 if (preg_match('#^/offer/new/restauration#', $uri)) {
-    $type = 'restauration';
+    $category = 'restauration';
     $description = "Décrivez ce que vous proposez (pour combien de personnes ?
 sans porc/végétarien/végétalien/hallal/casher/sans gluten ?
 sur place ou à emporter ? etc.)
 ";
 } elseif (preg_match('#^/offer/new/blanchisserie#', $uri)) {
-    $type = 'blanchisserie';
+    $category = 'blanchisserie';
     $description = "Décrivez ce que vous proposez (lessive fournie ?
 séchage sur place ? etc.)
 ";
 } elseif (preg_match('#^/offer/new/mobilite#', $uri)) {
-    $type = 'mobilite';
+    $category = 'mobilite';
     $description = "Décrivez ce que vous proposez (temps disponible ?
 place dans votre véhicule ? etc.)
 ";
 } elseif (preg_match('#^/offer/new/loisir#', $uri)) {
-    $type = 'loisir';
+    $category = 'loisir';
     $description = "Décrivez ce que vous proposez (activité ? pour qui ?
 où ? nombre de places ? etc.)
 ";
 } elseif (preg_match('#^/offer/new/don#', $uri)) {
-    $type = 'don';
+    $category = 'don';
     $description = "Décrivez ce que vous donnez (taille ? poids ?
 où le récupérez ? etc.)
 ";
@@ -51,7 +51,7 @@ où le récupérez ? etc.)
 
 <form action='/offer/new' method='post'>
 Titre : <input type='text' name='titre'>*<br>
-Type de l'offre : <?php print $category; ?><br>
+Catégorie de l'offre : <?php print $category; ?><br>
 Adresse : <input type='text' name='adresse'><br>
 Désactivation de l'offer le : <input type='text'><br>
 <?php print $description ?> <br>
