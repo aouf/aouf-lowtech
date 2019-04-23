0f28359c8cc8e9ae7b20e8d9eaeaaa4240384fd4
Juliette Cougnoux <jcougnoux+git@evolix.fr>
2019-04-23 18:36:25 +0200

    input plus grand index

--- tpl/index.php
+++ tpl/index.php
@@ -8,19 +8,19 @@ require_once 'head.php';
 
 ?>
 <body>
-    
-<div class='home flex column center'>
-    <div id='logo-accueil-deco'></div>
-    <div id='connexion-div' class='flex column center'>
-        <form id='connexion-form' class='flex center column' action='/auth' method='post'>
-            <label for='login'>Identifiant</label>
-            <input type='text' name='login' autofocus placeholder=''>
-            <label for='password'>Mot de passe</label>
-            <input type='password' name='password'>
-            <button class='bg-green' type='submit' value='Connexion'>Connexion</button>
-        </form>
-        <a href='/register'>S'inscrire</a>
+    <div class='home flex column center'>
+        <div class="content">
+            <div id='logo-accueil-deco'></div>
+            <div id='connexion-div' class='flex column center'>
+                <form id='connexion-form' class='flex center column' action='/auth' method='post'>
+                    <label for='login'>Identifiant</label>
+                    <input type='text' name='login' autofocus placeholder=''>
+                    <label for='password'>Mot de passe</label>
+                    <input type='password' name='password'>
+                    <button class='bg-green' type='submit' value='Connexion'>Connexion</button>
+                </form>
+                <center><a href='/register'>S'inscrire</a></center>
+            </div>
+        </div>
     </div>
-</div>
-
 </body>
