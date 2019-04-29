47e83285b64d2c8b1eab51dfe8a9814a4cc59ecb
Gregory Colpart <gcolpart+git@evolix.fr>
2019-04-30 01:12:15 +0200

    Amélioration de la création/édition des offres

--- tpl/offer_edit.php
+++ tpl/offer_edit.php
@@ -22,16 +22,40 @@ if (isset($_POST['title'])) {
     $title = $_POST['title'];
     $description = $_POST['description'];
     $status = $_POST['status'];
-    $req = "UPDATE offers SET title='$title',description='$description',status='$status' WHERE id=$offer_id";
+    $arrondissement = $_POST['arrondissement'];
+    $req = "UPDATE offers SET title='$title',description='$description',status='$status',arrondissement='$arrondissement' WHERE id=$offer_id";
     $statement = $pdo->prepare($req);
     $statement->execute();
+
+    // on met a jour le lastactivity de l'utilisateur
+    $lastactivity = date('Y-m-d H:i:s');
+    $req = "UPDATE users set date_lastactivity = $lastactivity WHERE id = $user_id";
+    $statement = $pdo->prepare($req);
+    $statement->execute();
+
+    // notification par email
+    $headers_mail = "MIME-Version: 1.0\n";
+    $headers_mail .= 'From: '.$conf['mail']['from']."\n";
+    $headers_mail .= 'Content-Type: text/plain; charset="utf-8"'."\n";
+    $body_mail = "Bonjour,
+
+Modification offre postée par l'utilisateur $user_id :
+
+$title
+
+$description
+
+--
+L'equipe Aouf
+";
+    mail($conf['mail']['admin'],'[aouf] Modification offre',$body_mail,$headers_mail);
+
+    echo "Offre <strong>$title</strong> modifiée&nbsp;!<br>";
 }
 
 ?>
 <div class="container bg-blanc noir full-size">
     <div class="content">
-
-        <h2>Edition offre</h2>
 
         <?php
 
@@ -44,33 +68,57 @@ if (isset($_POST['title'])) {
         $offer_description = $data['description'];
         $offer_status = $data['status'];
         $offer_userid = $data['user_id'];
-
+        $offer_arrondissement = $data['arrondissement'];
+        $offer_address = $data['address'];
+        list($offer_datestart,$offer_timestart) = preg_split("/ /", $data['date_start']);
+        list($offer_dateend,$offer_timeend) = preg_split("/ /", $data['date_end']);
         ?>
+
+        <h2>Édition de mon offre de <?php echo $offer_category; ?></h2>
 
         <form class="full-size flex center column" method='post' enctype='multipart/form-data'>
         <section>
-            <input type='radio' name='status' value='enabled' <?php if ($offer_status == 'enabled') print "checked"; ?>>Offre active
-            &nbsp;<input type='radio' name='status' value='disabled' <?php if ($offer_status == 'disabled') print "checked"; ?>>Offre inactive<br>
+            <input type='radio' name='status' value='enabled' <?php if ($offer_status == 'enabled') print "checked"; ?>> Offre active
+            &nbsp;<input type='radio' name='status' value='disabled' <?php if ($offer_status == 'disabled') print "checked"; ?>> Offre inactive<br>
         </section>
         <label for="title">Titre <span class="saumon">*</span></label><input type='text' name='title' value='<?php print $offer_title; ?>'>
-        <p>Catégorie de l'offre : <?php echo $offer_category; ?></p>
-        <label for="address">Adresse</label><input type='text' name='address'>
-        <label for="allDay">Toute la journée <input type="checkbox" name="allDay" value="yes"></label>
+        <label for="">Arrondissement (Marseille)<span class="saumon">*</span></label>
+        <select name='arrondissement'>
+            <option value='1' <?php if ($offer_arrondissement == 1) print "selected='selected'"; ?>>Marseille 1er</option>
+            <option value='2' <?php if ($offer_arrondissement == 2) print "selected='selected'"; ?>>Marseille 2eme</option>
+            <option value='3' <?php if ($offer_arrondissement == 3) print "selected='selected'"; ?>>Marseille 3eme</option>
+            <option value='4' <?php if ($offer_arrondissement == 4) print "selected='selected'"; ?>>Marseille 4eme</option>
+            <option value='5' <?php if ($offer_arrondissement == 5) print "selected='selected'"; ?>>Marseille 5eme</option>
+            <option value='6' <?php if ($offer_arrondissement == 6) print "selected='selected'"; ?>>Marseille 6eme</option>
+            <option value='7' <?php if ($offer_arrondissement == 7) print "selected='selected'"; ?>>Marseille 7eme</option>
+            <option value='8' <?php if ($offer_arrondissement == 8) print "selected='selected'"; ?>>Marseille 8eme</option>
+            <option value='9' <?php if ($offer_arrondissement == 9) print "selected='selected'"; ?>>Marseille 9eme</option>
+            <option value='10' <?php if ($offer_arrondissement == 10) print "selected='selected'"; ?>>Marseille 10eme</option>
+            <option value='11' <?php if ($offer_arrondissement == 11) print "selected='selected'"; ?>>Marseille 11eme</option>
+            <option value='12' <?php if ($offer_arrondissement == 12) print "selected='selected'"; ?>>Marseille 12eme</option>
+            <option value='13' <?php if ($offer_arrondissement == 13) print "selected='selected'"; ?>>Marseille 13eme</option>
+            <option value='14' <?php if ($offer_arrondissement == 14) print "selected='selected'"; ?>>Marseille 14eme</option>
+            <option value='15' <?php if ($offer_arrondissement == 15) print "selected='selected'"; ?>>Marseille 15eme</option>
+            <option value='16' <?php if ($offer_arrondissement == 16) print "selected='selected'"; ?>>Marseille 16eme</option>
+        </select>
+        <label for="address">Adresse (facultative)</label><input type='text' name='address' value='<?php print $offer_address; ?>' placeholder="Je donne l'adresse où se trouve mon offre">
+        <!--<label for="allDay">Toute la journée <input type="checkbox" name="allDay" value="yes"></label>-->
         <section class="flex column center">
             <span>Début de l'offre</span>
                 <section class="flex">
-                    <section class="flex column center"><label for="dateStart">Jour</label><input type='date' name="dateStart" value="<?php echo date('Y-m-d') ;?>"></section>
-                    <section class="flex column center"><label for="timeStart">Heure</label><input type='time' name="timeStart" value=""></section>
+                    <section class="flex column center"><label for="dateStart">Jour</label><input type='date' name="dateStart" value="<?php echo $offer_datestart; ?>"></section>
+                    <section class="flex column center"><label for="timeStart">Heure</label><input type='time' name="timeStart" value="<?php echo preg_replace('/(\d\d:\d\d):\d\d/','$1',$offer_timestart); ?>"></section>
                 </section>
         </section>
         <section class="flex column center">
             <span>Fin de l'offre</span>
                 <section class="flex">
-                    <section class="flex column center"><label for="dateEnd">Jour</label><input type='date' name="dateEnd" value="<?php echo date('Y-m-d', time() + 86400); ?>"></section>
-                    <section class="flex column center"><label for="timeEnd">Heure</label><input type='time' name="timeEnd" value=""></section>
+                    <section class="flex column center"><label for="dateEnd">Jour</label><input type='date' name="dateEnd" value="<?php echo $offer_dateend; ?>"></section>
+                    <section class="flex column center"><label for="timeEnd">Heure</label><input type='time' name="timeEnd" value="<?php echo preg_replace('/(\d\d:\d\d):\d\d/','$1',$offer_timeend); ?>"></section>
                 </section>
         </section>
         <textarea name='description'><?php echo $offer_description; ?></textarea>
+        <?php if ($offer_picture != '') { ?><label for="">Photo illustrant l'offre</label><input type='file' name='picture'> <?php } ?>
         <button class='bg-vert noir' type="submit" name="button" value"Modifier">Modifier</button>
         </form>
 
--- tpl/offer_new.php
+++ tpl/offer_new.php
@@ -15,24 +15,55 @@ if (isset($_POST['title'])) {
     $title = $_POST['title'];
     $arrondissement = $_POST['arrondissement'];
     $address = $_POST['address'];
-    $description = $_POST['description1'].$_POST['description2'].$_POST['description3'];
-    $picture = file_get_contents($_FILES['picture']['tmp_name']);
+    $date_start = $_POST['dateStart'].' '.$_POST['timeStart'];
+    $date_end = $_POST['dateEnd'].' '.$_POST['timeEnd'];
+    $description = $_POST['description1']."\n".$_POST['description2']."\n".$_POST['description3'];
+    $picture = (($_FILES['picture']['tmp_name']) ? file_get_contents($_FILES['picture']['tmp_name']) : 'NULL');
 
-    $req = "INSERT INTO offers(user_id,category,title,description,status,arrondissement,address,picture) VALUES (?,?,?,?,?,?,?,?)";
+    // insertion de l'offre en base
+    $req = "INSERT INTO offers(user_id,category,title,description,status,date_start,date_end,arrondissement,address,picture) VALUES (?,?,?,?,?,?,?,?,?,?)";
     $statement = $pdo->prepare($req);
-    $statement->execute([$user_id, $category, $title, $description,'enabled', $arrondissement, $address, $picture]);
+    $statement->execute([$user_id,$category,$title,$description,'enabled',$date_start,$date_end,$arrondissement,$address,$picture]);
 
-    echo "Annonce <strong>$title</strong> postée, merci&nbsp;!<br>";
+    // on met a jour le lastactivity de l'utilisateur
+    $lastactivity = date('Y-m-d H:i:s');
+    $req = "UPDATE users set date_lastactivity = $lastactivity WHERE id = $user_id";
+    $statement = $pdo->prepare($req);
+    $statement->execute();
+
+    // notification par email
+    $headers_mail = "MIME-Version: 1.0\n";
+    $headers_mail .= 'From: '.$conf['mail']['from']."\n";
+    $headers_mail .= 'Content-Type: text/plain; charset="utf-8"'."\n";
+    $body_mail = "Bonjour,
+
+Nouvelle offre postée par l'utilisateur $user_id :
+
+$title
+
+$description
+
+--
+L'equipe Aouf
+";
+    mail($conf['mail']['admin'],'[aouf] Nouvelle offre',$body_mail,$headers_mail);
+
+    echo "Offre <strong>$title</strong> postée, merci&nbsp;!<br>";
 }
 
 $uri = $_SERVER['REQUEST_URI'];
-$description = "Décrivez ce que vous proposez.";
+$placeholdertitre = "Ce que je propose en 1 ligne…";
+$description = "Décrivez ce que vous proposez";
+$placeholder1 = "Je propose…";
+$placeholder2 = "";
+$placeholder3 = "";
 if (preg_match('#^/offer/new/restauration#', $uri)) {
     $category = 'restauration';
-    $description = "Décrivez ce que vous proposez (pour combien de personnes ?
-sans porc / végétarien / végétalien / hallal / casher / sans gluten ?
-sur place ou à emporter ? etc.)
-";
+    $placeholdertitre = "Couscous végétarien, mercredi midi, pour 6 personnes";
+    $description = "Détails sur l'offre de restauration";
+    $placeholder1 = "Je propose un repas complet pour 3 personnes…";
+    $placeholder2 = "Le repas peut être consommé sur place ou à emporter…";
+    $placeholder3 = "Le repas est de type sans porc / végétarien / végétalien / hallal / casher / sans gluten…";
 } elseif (preg_match('#^/offer/new/blanchisserie#', $uri)) {
     $category = 'blanchisserie';
     $description = "Décrivez ce que vous proposez (lessive fournie ?
@@ -58,53 +89,52 @@ où le récupérez ? etc.)
 <div class="container bg-blanc noir full-size">
     <div class="content">
 
-        <h2>Proposer une offre</h2>
+        <h2>Je propose une offre de <?php echo $category; ?></h2>
             <form class="full-size flex center column" method='post' enctype='multipart/form-data'>
             <label for="title">Titre <span class="saumon">*</span></label>
-            <input type='text' name='title'>
-            <p>Catégorie de l'offre : <?php echo $category; ?></p>
-            <label for="">Arrondissement<span class="saumon">*</span></label>
+            <input type='text' name='title' placeholder='<?php echo $placeholdertitre; ?>'>
+            <label for="">Arrondissement (Marseille)<span class="saumon">*</span></label>
             <select name='arrondissement'>
-                <option value='0'>-</option>
-                <option value='1'>1er</option>
-                <option value='2'>2eme</option>
-                <option value='3'>3eme</option>
-                <option value='4'>4eme</option>
-                <option value='5'>5eme</option>
-                <option value='6'>6eme</option>
-                <option value='7'>7eme</option>
-                <option value='8'>8eme</option>
-                <option value='9'>9eme</option>
-                <option value='10'>10eme</option>
-                <option value='11'>11eme</option>
-                <option value='12'>12eme</option>
-                <option value='13'>13eme</option>
-                <option value='14'>14eme</option>
-                <option value='15'>15eme</option>
-                <option value='16'>16eme</option>
+                <option value='0' selected='selected' disabled='disabled'>Je choisis l'arrondissement où se trouve mon offre</option>
+                <option value='1'>Marseille 1er</option>
+                <option value='2'>Marseille 2eme</option>
+                <option value='3'>Marseille 3eme</option>
+                <option value='4'>Marseille 4eme</option>
+                <option value='5'>Marseille 5eme</option>
+                <option value='6'>Marseille 6eme</option>
+                <option value='7'>Marseille 7eme</option>
+                <option value='8'>Marseille 8eme</option>
+                <option value='9'>Marseille 9eme</option>
+                <option value='10'>Marseille 10eme</option>
+                <option value='11'>Marseille 11eme</option>
+                <option value='12'>Marseille 12eme</option>
+                <option value='13'>Marseille 13eme</option>
+                <option value='14'>Marseille 14eme</option>
+                <option value='15'>Marseille 15eme</option>
+                <option value='16'>Marseille 16eme</option>
             </select>
-            <label for="address">Adresse</label>
-            <input type='text' name='address'>
-            <label for="allDay">Toute la journée <input type="checkbox" name="allDay" value="yes"></label>
+            <label for="address">Adresse (facultatif)</label>
+            <input type='text' name='address' placeholder="Je donne l'adresse où se trouve mon offre">
+            <!--<label for="allDay">Toute la journée <input type="checkbox" name="allDay" value="yes"></label>-->
             <section class="flex column center">
-                <span>Début de l'offre</span>
+                <span>Début de l'offre <span class="saumon">*</span></span>
                     <section class="flex">
-                        <section class="flex column center"><label for="dateStart">Jour</label><input type='date' name="dateStart" value="<?php echo date('Y-m-d') ;?>"></section>
-                        <section class="flex column center"><label for="timeStart">Heure</label><input type='time' name="timeStart" value=""></section>
+                        <section class="flex column center"><label for="dateStart">Jour</label><input type='date' name="dateStart" value="<?php echo date('Y-m-d'); ?>"></section>
+                        <section class="flex column center"><label for="timeStart">Heure</label><input type='time' name="timeStart" value="<?php echo date('H:i'); ?>"></section>
                     </section>
             </section>
             <section class="flex column center">
-                <span>Fin de l'offre</span>
+                <span>Fin de l'offre <span class="saumon">*</span></span>
                     <section class="flex">
-                        <section class="flex column center"><label for="dateEnd">Jour</label><input type='date' name="dateEnd" value="<?php echo date('Y-m-d', time() + 86400); ?>"></section>
-                        <section class="flex column center"><label for="timeEnd">Heure</label><input type='time' name="timeEnd" value=""></section>
+                        <section class="flex column center"><label for="dateEnd">Jour</label><input type='date' name="dateEnd" value="<?php echo date('Y-m-d', time() + 7200); ?>"></section>
+                        <section class="flex column center"><label for="timeEnd">Heure</label><input type='time' name="timeEnd" value="<?php echo date('H:i', time() + 7200); ?>"></section>
                     </section>
             </section>
             <p><?php echo $description ?> <span class="saumon">*</span></p>
-            <textarea name='description1' placeholder="Description 1 ici…"></textarea>
-            <textarea name='description2' placeholder="Description 2 ici…"></textarea>
-            <textarea name='description3' placeholder="Description 3 ici…"></textarea>
-            <label for="">Photo</label><input type='file' name='picture'>
+            <textarea name='description1' placeholder="<?php echo $placeholder1; ?>"></textarea>
+            <textarea name='description2' placeholder="<?php echo $placeholder2; ?>"></textarea>
+            <textarea name='description3' placeholder="<?php echo $placeholder3; ?>"></textarea>
+            <label for="">Photo illustrant l'offre</label><input type='file' name='picture'>
             <input type='hidden' name='category' value='<?php echo $category; ?>'>
             <button class='bg-vert noir' type="submit" name="button" value"Publier">Publier</button>
             </form>
