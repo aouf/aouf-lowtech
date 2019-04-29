a493b691dc1330c07a625d46901e457a32a81548
Juliette Cougnoux <jcougnoux+git@evolix.fr>
2019-04-29 19:49:47 +0200

    Amélioration des formulaires new et edit

--- tpl/offer_edit.php
+++ tpl/offer_edit.php
@@ -29,34 +29,51 @@ if (isset($_POST['title'])) {
 
 ?>
 <div class="container bg-blanc noir full-size">
-    <h2>Edition offre</h2>
-
-    <?php
-
-    $req = "SELECT * FROM offers where id = $offer_id LIMIT 1";
-    $statement = $pdo->query($req);
-    $data = $statement->fetch();
-    $offer_title = $data['title'];
-    $offer_category = $data['category'];
-    $offer_description = $data['description'];
-    $offer_description = $data['description'];
-    $offer_status = $data['status'];
-    $offer_userid = $data['user_id'];
-
-    ?>
-
-    <form method='post'>
-    <input type='radio' name='status' value='enabled' <?php if ($offer_status == 'enabled') print "checked"; ?>>Offre active
-    <input type='radio' name='status' value='disabled' <?php if ($offer_status == 'disabled') print "checked"; ?>>Offre inactive<br>
-    Titre : <input type='text' name='title' value='<?php print $offer_title; ?>'>*<br>
-    Catégorie de l'offre : <?php print $offer_category; ?><br>
-    Adresse : <input type='text' name='address'><br>
-    Désactivation de l'offre le : <input type='text'><br>
-    <textarea name='description'><?php print $offer_description; ?></textarea>
-    <br><br>
-    <input type='submit' value='Modifier'>
-    </form>
     <div class="content">
+
+        <h2>Edition offre</h2>
+
+        <?php
+
+        $req = "SELECT * FROM offers where id = $offer_id LIMIT 1";
+        $statement = $pdo->query($req);
+        $data = $statement->fetch();
+        $offer_title = $data['title'];
+        $offer_category = $data['category'];
+        $offer_description = $data['description'];
+        $offer_description = $data['description'];
+        $offer_status = $data['status'];
+        $offer_userid = $data['user_id'];
+
+        ?>
+
+        <form class="full-size flex center column" method='post' enctype='multipart/form-data'>
+        <section>
+            <input type='radio' name='status' value='enabled' <?php if ($offer_status == 'enabled') print "checked"; ?>>Offre active
+            &nbsp;<input type='radio' name='status' value='disabled' <?php if ($offer_status == 'disabled') print "checked"; ?>>Offre inactive<br>
+        </section>
+        <label for="title">Titre <span class="saumon">*</span></label><input type='text' name='title' value='<?php print $offer_title; ?>'>
+        <p>Catégorie de l'offre : <?php echo $offer_category; ?></p>
+        <label for="address">Adresse</label><input type='text' name='address'>
+        <label for="allDay">Toute la journée <input type="checkbox" name="allDay" value="yes"></label>
+        <section class="flex column center">
+            <span>Début de l'offre</span>
+                <section class="flex">
+                    <section class="flex column center"><label for="dateStart">Jour</label><input type='date' name="dateStart" value="<?php echo date('Y-m-d') ;?>"></section>
+                    <section class="flex column center"><label for="timeStart">Heure</label><input type='time' name="timeStart" value=""></section>
+                </section>
+        </section>
+        <section class="flex column center">
+            <span>Fin de l'offre</span>
+                <section class="flex">
+                    <section class="flex column center"><label for="dateEnd">Jour</label><input type='date' name="dateEnd" value="<?php echo date('Y-m-d', time() + 86400); ?>"></section>
+                    <section class="flex column center"><label for="timeEnd">Heure</label><input type='time' name="timeEnd" value=""></section>
+                </section>
+        </section>
+        <textarea name='description'><?php echo $offer_description; ?></textarea>
+        <button class='bg-vert noir' type="submit" name="button" value"Modifier">Modifier</button>
+        </form>
+
     </div>
 </div>
 
--- tpl/offer_new.php
+++ tpl/offer_new.php
@@ -30,7 +30,7 @@ $description = "Décrivez ce que vous proposez.";
 if (preg_match('#^/offer/new/restauration#', $uri)) {
     $category = 'restauration';
     $description = "Décrivez ce que vous proposez (pour combien de personnes ?
-sans porc/végétarien/végétalien/hallal/casher/sans gluten ?
+sans porc / végétarien / végétalien / hallal / casher / sans gluten ?
 sur place ou à emporter ? etc.)
 ";
 } elseif (preg_match('#^/offer/new/blanchisserie#', $uri)) {
@@ -56,53 +56,58 @@ où le récupérez ? etc.)
 } 
 ?>
 <div class="container bg-blanc noir full-size">
-<h2>Proposer une offre</h2>
-    <form class="full-size flex center column" method='post' enctype='multipart/form-data'>
-    <label for="title">Titre <span class="saumon">*</span></label><input type='text' name='title'>
-    <p>Catégorie de l'offre : <?php print $category; ?></p>
-    <label for="">Arrondissement<span class="saumon">*</span></label><select name='arrondissement'>
-    <option value='0'>-</option>
-    <option value='1'>1er</option>
-    <option value='2'>2eme</option>
-    <option value='3'>3eme</option>
-    <option value='4'>4eme</option>
-    <option value='5'>5eme</option>
-    <option value='6'>6eme</option>
-    <option value='7'>7eme</option>
-    <option value='8'>8eme</option>
-    <option value='9'>9eme</option>
-    <option value='10'>10eme</option>
-    <option value='11'>11eme</option>
-    <option value='12'>12eme</option>
-    <option value='13'>13eme</option>
-    <option value='14'>14eme</option>
-    <option value='15'>15eme</option>
-    <option value='16'>16eme</option></select>
-    <label for="address">Adresse</label><input type='text' name='address'>
-    <label for="allDay">Toute la journée <input type="checkbox" name="allDay" value="yes"></label>
-    <section class="flex column center">
-        <span>Début de l'offre</span>
-            <section class="flex">
-                <section class="flex column center"><label for="dateStart">Jour</label><input type='date' name="dateStart" value="<?php echo date('Y-m-d') ;?>"></section>
-                <section class="flex column center"><label for="timeStart">Heure</label><input type='time' name="timeStart" value=""></section>
     <div class="content">
+
+        <h2>Proposer une offre</h2>
+            <form class="full-size flex center column" method='post' enctype='multipart/form-data'>
+            <label for="title">Titre <span class="saumon">*</span></label>
+            <input type='text' name='title'>
+            <p>Catégorie de l'offre : <?php echo $category; ?></p>
+            <label for="">Arrondissement<span class="saumon">*</span></label>
+            <select name='arrondissement'>
+                <option value='0'>-</option>
+                <option value='1'>1er</option>
+                <option value='2'>2eme</option>
+                <option value='3'>3eme</option>
+                <option value='4'>4eme</option>
+                <option value='5'>5eme</option>
+                <option value='6'>6eme</option>
+                <option value='7'>7eme</option>
+                <option value='8'>8eme</option>
+                <option value='9'>9eme</option>
+                <option value='10'>10eme</option>
+                <option value='11'>11eme</option>
+                <option value='12'>12eme</option>
+                <option value='13'>13eme</option>
+                <option value='14'>14eme</option>
+                <option value='15'>15eme</option>
+                <option value='16'>16eme</option>
+            </select>
+            <label for="address">Adresse</label>
+            <input type='text' name='address'>
+            <label for="allDay">Toute la journée <input type="checkbox" name="allDay" value="yes"></label>
+            <section class="flex column center">
+                <span>Début de l'offre</span>
+                    <section class="flex">
+                        <section class="flex column center"><label for="dateStart">Jour</label><input type='date' name="dateStart" value="<?php echo date('Y-m-d') ;?>"></section>
+                        <section class="flex column center"><label for="timeStart">Heure</label><input type='time' name="timeStart" value=""></section>
+                    </section>
             </section>
-    </section>
-    <section class="flex column center">
-        <span>Fin de l'offre</span>
-            <section class="flex">
-                <section class="flex column center"><label for="dateEnd">Jour</label><input type='date' name="dateEnd" value="<?php echo date('Y-m-d', time() + 86400); ?>"></section>
-                <section class="flex column center"><label for="timeEnd">Heure</label><input type='time' name="timeEnd" value=""></section>
+            <section class="flex column center">
+                <span>Fin de l'offre</span>
+                    <section class="flex">
+                        <section class="flex column center"><label for="dateEnd">Jour</label><input type='date' name="dateEnd" value="<?php echo date('Y-m-d', time() + 86400); ?>"></section>
+                        <section class="flex column center"><label for="timeEnd">Heure</label><input type='time' name="timeEnd" value=""></section>
+                    </section>
             </section>
-    </section>
-    <?php print $description ?> <br>
-    <textarea name='description1'></textarea><br>
-    <textarea name='description2'></textarea><br>
-    <textarea name='description3'></textarea>*<br>
-    <label for="">Photo</label><input type='file' name='picture'>
-    <input type='hidden' name='category' value='<?php print $category; ?>'>
-    <input type='submit' value='Publier'>
-    </form>
+            <p><?php echo $description ?> <span class="saumon">*</span></p>
+            <textarea name='description1' placeholder="Description 1 ici…"></textarea>
+            <textarea name='description2' placeholder="Description 2 ici…"></textarea>
+            <textarea name='description3' placeholder="Description 3 ici…"></textarea>
+            <label for="">Photo</label><input type='file' name='picture'>
+            <input type='hidden' name='category' value='<?php echo $category; ?>'>
+            <button class='bg-vert noir' type="submit" name="button" value"Publier">Publier</button>
+            </form>
 
     </div>
 </div>
