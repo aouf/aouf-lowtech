5f4c753d0b78f63103350a91fe4a0607e0761ec6
Juliette Cougnoux <jcougnoux+git@evolix.fr>
2019-04-29 15:18:41 +0200

    Modif icones accueil

--- tpl/accueil.php
+++ tpl/accueil.php
@@ -23,22 +23,28 @@ require_once 'header.php';
             <h2 class="blanc">Consulter les offres</h2>
             <section class="tableau-de-bord flex center wrap">
                 <a class="flex center column" href='/offer/list/restauration'>
-                    <img class="icone" src="./images/restauration.png" alt="">
+                    <img class="icone" src="./images/restauration-saumon.png" alt="">
+                    <h3>Restauration</h3>
                 </a>
-                <a class="flex center" href='/offer/list/blanchisserie'>
-                    <img class="icone" src="./images/blanchisserie.png" alt="">
+                <a class="flex center column" href='/offer/list/blanchisserie'>
+                    <img class="icone" src="./images/blanchisserie-saumon.png" alt="">
+                    <h3>Blanchisserie</h3>
                 </a>
-                <a class="flex center" href='/offer/list/mobilite'>
-                    <img class="icone" src="./images/mobilite.png" alt="">
+                <a class="flex center column" href='/offer/list/mobilite'>
+                    <img class="icone" src="./images/mobilite-saumon.png" alt="">
+                    <h3>Mobilté</h3>
                 </a>
-                <a class="flex center" href='/offer/list/loisir'>
-                    <img class="icone" src="./images/loisir.png" alt="">
+                <a class="flex center column" href='/offer/list/loisir'>
+                    <img class="icone" src="./images/loisir-saumon.png" alt="">
+                    <h3>Loisir</h3>
                 </a>
-                <a class="flex center" href='/offer/list/don'>
-                    <img class="icone" src="./images/dons.png" alt="">
+                <a class="flex center column" href='/offer/list/don'>
+                    <img class="icone" src="./images/dons-saumon.png" alt="">
+                    <h3>Dons</h3>
                 </a>
-                <a class="flex center" href='/offer/list/autre'>
-                    <img class="icone" src="./images/autre.png" alt="">
+                <a class="flex center column" href='/offer/list/autre'>
+                    <img class="icone" src="./images/autre-saumon.png" alt="">
+                    <h3>Autre</h3>
                 </a>
             </section>
             
@@ -51,23 +57,29 @@ require_once 'header.php';
 
             <h2 class="blanc">Déposer une offre</h2>
             <section class="tableau-de-bord flex center wrap">
-                <a class="flex center" href='/offer/new/restauration'>
-                    <img class="icone" src="./images/restauration.png" alt="">
+                <a class="flex center column" href='/offer/new/restauration'>
+                    <img class="icone" src="./images/restauration-vert.png" alt="">
+                    <h3>Restauration</h3>
                 </a>
-                <a class="flex center" href='/offer/new/blanchisserie'>
-                    <img class="icone" src="./images/blanchisserie.png" alt="">
+                <a class="flex center column" href='/offer/new/blanchisserie'>
+                    <img class="icone" src="./images/blanchisserie-vert.png" alt="">
+                    <h3>Blanchisserie</h3>
                 </a>
-                <a class="flex center" href='/offer/new/mobilite'>
-                    <img class="icone" src="./images/mobilite.png" alt="">
+                <a class="flex center column" href='/offer/new/mobilite'>
+                    <img class="icone" src="./images/mobilite-vert.png" alt="">
+                    <h3>Mobilté</h3>
                 </a>
-                <a class="flex center" href='/offer/new/loisir'>
-                    <img class="icone" src="./images/loisir.png" alt="">
+                <a class="flex center column" href='/offer/new/loisir'>
+                    <img class="icone" src="./images/loisir-vert.png" alt="">
+                    <h3>Loisir</h3>
                 </a>
-                <a class="flex center" href='/offer/new/don'>
-                    <img class="icone" src="./images/dons.png" alt="">
+                <a class="flex center column" href='/offer/new/don'>
+                    <img class="icone" src="./images/dons-vert.png" alt="">
+                    <h3>Dons</h3>
                 </a>
-                <a class="flex center" href='/offer/new/autre'>
-                    <img class="icone" src="./images/autre.png" alt="">
+                <a class="flex center column" href='/offer/new/autre'>
+                    <img class="icone" src="./images/autre-vert.png" alt="">
+                    <h3>Autre</h3>
                 </a>
             </section>
             
