e5d6238b614131a0cc64ec41f0cc80a59bbe1415
Juliette Cougnoux <jcougnoux+git@evolix.fr>
2019-05-07 17:59:34 +0200

    Début design pour les offres

--- tpl/offer_list.php
+++ tpl/offer_list.php
@@ -16,79 +16,114 @@ $arrondissement = $_SESSION['user_arrondissement'];
 $pdo = new PDO('mysql:host='.SERVEUR.';dbname='.BASE,NOM,PASSE);
 ?>
 <div class="container bg-blanc noir full-size">
-    <h2>Liste des offres de <?php echo $offer_category; ?></h2>
+    <h2 class="saumon"><?php echo ucfirst($offer_category); ?></h2>
+    <div class="bg-saumon full-size list-offres">
+        <?php
+        if ($arrondissement != '') {
 
-    <?php
-    if ($arrondissement != '') {
-        print "<h3>Offres dans votre arrondissement ($arrondissement)</h3>";
-        $req = "SELECT offers.id,offers.user_id,offers.title,offers.description,offers.arrondissement,offers.picture,users.name,users.firstname FROM offers,users WHERE offers.user_id = users.id AND offers.category = '$offer_category' AND offers.status='enabled' AND users.status='enabled' AND offers.arrondissement=$arrondissement";
-        $statement = $pdo->query($req);
-
-        while ($data = $statement->fetch()) {
-
-            $offer_id = $data['id'];
-            $offer_userid = $data['user_id'];
-            $titre = $data['title'];
-            $description = $data['description'];
-            $arrondissement = $data['arrondissement'];
-            $name = $data['name'];
-            $firstname = $data['firstname'];
+            // print "<h3>Offres dans votre arrondissement ($arrondissement)</h3>";
+            $req = "SELECT offers.id, offers.user_id, offers.title, offers.description, offers.arrondissement, offers.picture, offers.date_start,  offers.date_end, users.name, users.firstname FROM offers,users WHERE offers.user_id = users.id AND offers.category = '$offer_category' AND offers.status='enabled' AND users.status='enabled' AND offers.arrondissement=$arrondissement";
+            $statement = $pdo->query($req);
             
-            echo "Annonce $offer_id : $titre - desc: $description - from: $firstname $name - arr: $arrondissement<br>";
-            if ($data['picture'] != 'NULL') {
-                $picture = base64_encode($data['picture']);
-                print "<img src='data:image/jpg;base64,$picture' width='200'><br>";
+            while ($data = $statement->fetch()) {
+                $offer_id = $data['id'];
+                $offer_userid = $data['user_id'];
+                $titre = $data['title'];
+                $description = $data['description'];
+                $arrondissement = $data['arrondissement'];
+                $name = $data['name'];
+                $firstname = $data['firstname'];
+                $debut = date('d/m/y', strtotime($data['date_start']));
+                $fin = date('d/m/y', strtotime($data['date_end']));
+                $arrondissement == '1' ? $arrondissement .= 'er' : $arrondissement .= 'ème' ;
+
+                echo "<a class='offre' href='/message/write/$offer_id/$offer_userid'>";
+                    echo "<div class='flex'>";
+                        echo "<div class='oblique-gauche bg-blanc'>";
+                            echo "<h3 class='noir'>".ucfirst($titre)."</h3>";
+                            echo "<p class='date-lieu saumon'>$debut - $fin - $arrondissement</p>";
+                            echo "<p class='description noir'>$description</p>";
+                        echo "</div>";
+                        echo "<div class='oblique-droite bg-blanc'>";
+                            if ($data['picture'] != 'NULL') {
+                                $picture = base64_encode($data['picture']);
+                                echo "<div class='img-offre' style='background-image: url(data:image/jpg;base64,$picture)''></div>";
+                            }
+                        echo "</div>";
+                    echo "</div>";
+                echo "</a>";
             }
-            echo "<a href='/message/write/$offer_id/$offer_userid'>Réserver cette offre</a><br><br>";
-
-        }
-        print "<h3>Offres dans les autres arrondissements</h3>";
-        $req = "SELECT offers.id,offers.user_id,offers.title,offers.description,offers.arrondissement,offers.picture,users.name,users.firstname FROM offers,users WHERE offers.user_id = users.id AND offers.category = '$offer_category' AND offers.status='enabled' AND users.status='enabled' AND offers.arrondissement!=$arrondissement";
-        $statement = $pdo->query($req);
-
-        while ($data = $statement->fetch()) {
-
-            $offer_id = $data['id'];
-            $offer_userid = $data['user_id'];
-            $titre = $data['title'];
-            $description = $data['description'];
-            $arrondissement = $data['arrondissement'];
-            $name = $data['name'];
-            $firstname = $data['firstname'];
+            // print "<h3>Offres dans les autres arrondissements</h3>";
+            $req = "SELECT offers.id, offers.user_id, offers.title, offers.description, offers.arrondissement, offers.picture, offers.date_start,  offers.date_end, users.name, users.firstname FROM offers,users WHERE offers.user_id = users.id AND offers.category = '$offer_category' AND offers.status='enabled' AND users.status='enabled' AND offers.arrondissement!=$arrondissement";
+            $statement = $pdo->query($req);
             
-            echo "Annonce $offer_id : $titre - desc: $description - from: $firstname $name - arr: $arrondissement<br>";
-            if ($data['picture'] != 'NULL') {
-                $picture = base64_encode($data['picture']);
-                print "<img src='data:image/jpg;base64,$picture' width='200'><br>";
+            while ($data = $statement->fetch()) {
+                // die(var_dump($data));
+                $offer_id = $data['id'];
+                $offer_userid = $data['user_id'];
+                $titre = $data['title'];
+                $description = $data['description'];
+                $arrondissement = $data['arrondissement'];
+                $name = $data['name'];
+                $firstname = $data['firstname'];
+                $debut = date('d/m/y', strtotime( $data['date_start']));
+                $fin = date('d/m/y', strtotime($data['date_end']));
+                $arrondissement == '1' ? $arrondissement .= 'er' : $arrondissement .= 'ème' ;
+
+                echo "<a class='offre' href='/message/write/$offer_id/$offer_userid'>";
+                    echo "<div class='flex'>";
+                        echo "<div class='oblique-gauche bg-blanc'>";
+                            echo "<h3 class='noir'>".ucfirst($titre)."</h3>";
+                            echo "<p class='date-lieu saumon'>$debut - $fin - $arrondissement</p>";
+                            echo "<p class='description noir'>$description</p>";
+                        echo "</div>";
+                        echo "<div class='oblique-droite bg-blanc'>";
+                            if ($data['picture'] != 'NULL') {
+                                $picture = base64_encode($data['picture']);
+                                echo "<div class='img-offre' style='background-image: url(data:image/jpg;base64,$picture)''></div>";
+                            }
+                        echo "</div>";
+                    echo "</div>";
+                echo "</a>";
             }
-            echo "<a href='/message/write/$offer_id/$offer_userid'>Réserver cette offre</a><br><br>";
-
-        }
-    } else {
-
-        $req = "SELECT offers.id,offers.user_id,offers.title,offers.description,offers.arrondissement,offers.picture,users.name,users.firstname FROM offers,users WHERE offers.user_id = users.id AND offers.category = '$offer_category' AND offers.status='enabled' AND users.status='enabled'";
-        $statement = $pdo->query($req);
-
-        while ($data = $statement->fetch()) {
-
-            $offer_id = $data['id'];
-            $offer_userid = $data['user_id'];
-            $titre = $data['title'];
-            $description = $data['description'];
-            $arrondissement = $data['arrondissement'];
-            $name = $data['name'];
-            $firstname = $data['firstname'];
+        } else {
             
-            echo "Annonce $offer_id : $titre - desc: $description - from: $firstname $name - arr: $arrondissement<br>";
-            if ($data['picture'] != 'NULL') {
-                $picture = base64_encode($data['picture']);
-                print "<img src='data:image/jpg;base64,$picture' width='200'><br>";
+            $req = "SELECT offers.id, offers.user_id, offers.title, offers.description, offers.arrondissement, offers.picture, offers.date_start,  offers.date_end, users.name, users.firstname FROM offers,users WHERE offers.user_id = users.id AND offers.category = '$offer_category' AND offers.status='enabled' AND users.status='enabled'";
+            $statement = $pdo->query($req);
+            
+            while ($data = $statement->fetch()) {
+                
+                $offer_id = $data['id'];
+                $offer_userid = $data['user_id'];
+                $titre = $data['title'];
+                $description = $data['description'];
+                $arrondissement = $data['arrondissement'];
+                $name = $data['name'];
+                $firstname = $data['firstname'];
+                $debut = date('d/m/y', strtotime($data['date_start']));
+                $fin = date('d/m/y', strtotime( $data['date_end']));
+                $arrondissement == '1' ? $arrondissement .= 'er' : $arrondissement .= 'ème' ;
+                
+                echo "<a class='offre' href='/message/write/$offer_id/$offer_userid'>";
+                    echo "<div class='flex'>";
+                        echo "<div class='oblique-gauche bg-blanc'>";
+                            echo "<h3 class='noir'>".ucfirst($titre)."</h3>";
+                            echo "<p class='date-lieu saumon'>$debut - $fin - $arrondissement</p>";
+                            echo "<p class='description noir'>$description</p>";
+                        echo "</div>";
+                        echo "<div class='oblique-droite bg-blanc'>";
+                            if ($data['picture'] != 'NULL') {
+                                $picture = base64_encode($data['picture']);
+                            echo "<div class='img-offre' style='background-image: url(data:image/jpg;base64,$picture)''></div>";
+                            }
+                        echo "</div>";
+                    echo "</div>";
+                echo "</a>";
             }
-            echo "<a href='/message/write/$offer_id/$offer_userid'>Réserver cette offre</a><br><br>";
-
         }
-    }
-    ?>
+        ?>
+    </div>
+
 </div>
 <?php
     require_once 'footer.php';
