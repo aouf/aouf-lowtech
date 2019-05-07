b78ed1634bcb1f5200ebcdef8cb0e0cc71cc1c5d
Gregory Colpart <gcolpart+git@evolix.fr>
2019-05-08 01:05:13 +0200

    Application style minimum pour liste de messages

--- tpl/message_list.php
+++ tpl/message_list.php
@@ -6,21 +6,39 @@ require_once 'header.php';
 $user_id = $_SESSION['user_id'];
 ?>
 
-<h2>Liste des messages</h2>
+<div class="container bg-blanc noir full-size">
+<h2>Mes messages</h2>
+<div class="bg-saumon full-size list-offres">
 
 <?php
 $pdo = new PDO('mysql:host='.SERVEUR.';dbname='.BASE,NOM,PASSE);
 $req = "SELECT DISTINCT offer_id,from_id,to_id FROM messages WHERE from_id=$user_id OR to_id=$user_id";
 $statement = $pdo->query($req);
 
+$msg[0][0] = FALSE;
+
 while ($data = $statement->fetch()) {
 
+$message = "blabla";
 $offer_id = $data['offer_id'];
 $with_id = $data['from_id'];
 if ($with_id == $user_id) $with_id = $data['to_id'];
 
-echo "Offre $offer_id / message avec $with_id : <a href='/message/write/$offer_id/$with_id'>Voir messages</a><br>";
+if (!isset($msg[$offer_id][$with_id])) {
+    $msg[$offer_id][$with_id] = TRUE;
+            echo "<a class='offre' href='/message/write/$offer_id/$with_id'>";
+                    echo "<div class='flex'>";
+                        echo "<div class='full-size bg-blanc'>";
+                            echo "<h3 class='noir'>$with_id (offre $offer_id)</h3>";
+                            echo "<p class='description noir'>$message</p>";
+                        echo "</div>";
+                    echo "</div>";
+                echo "</a>";
+        }
+    }
 
-}
-
+    ?>
+    </div>
+</div>
+<?php
 require_once 'footer.php';
