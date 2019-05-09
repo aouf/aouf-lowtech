dde78a2f8363216f7c85d963be514aab28a33c96
Juliette Cougnoux <jcougnoux+git@evolix.fr>
2019-05-09 18:01:19 +0200

    liste des offres design test

--- tpl/offer_list.php
+++ tpl/offer_list.php
@@ -13,7 +13,9 @@ preg_match('#^/offer/list/(\w+)$#', $uri, $matches);
 $offer_category = trim($matches[1]);
 
 $arrondissement = $_SESSION['user_arrondissement'];
+$arrondissement == '1' ? $arrondissement_beautify = $arrondissement.'er' : $arrondissement_beautify = $arrondissement.'ème' ;
 $pdo = new PDO('mysql:host='.SERVEUR.';dbname='.BASE,NOM,PASSE);
+$max_length = 60;
 ?>
 <div class="container bg-blanc noir full-size">
     <h2 class="saumon"><?php echo ucfirst($offer_category); ?></h2>
@@ -21,15 +23,22 @@ $pdo = new PDO('mysql:host='.SERVEUR.';dbname='.BASE,NOM,PASSE);
         <?php
         if ($arrondissement != '') {
 
-            echo "<h3>Offres dans mon arrondissement</h3>";
+            echo "<h3>Offres dans votre arrondissement ($arrondissement_beautify)</h3>";
             $req = "SELECT offers.id, offers.user_id, offers.title, offers.description, offers.arrondissement, offers.picture, offers.date_start,  offers.date_end, users.name, users.firstname FROM offers,users WHERE offers.user_id = users.id AND offers.category = '$offer_category' AND offers.status='enabled' AND users.status='enabled' AND offers.arrondissement=$arrondissement";
             $statement = $pdo->query($req);
             
             while ($data = $statement->fetch()) {
                 $offer_id = $data['id'];
                 $offer_userid = $data['user_id'];
-                $titre = $data['title'];
+                $titre = ucfirst($data['title']);
+
                 $description = $data['description'];
+                if (strlen($description) > $max_length)
+                {
+                    $offset = ($max_length - 3) - strlen($description);
+                    $description = substr($description, 0, strrpos($description, ' ', $offset)) . '...';
+                }
+
                 $offer_arrondissement = $data['arrondissement'];
                 $offer_arrondissement == '1' ? $offer_arrondissement = $offer_arrondissement.'er' : $offer_arrondissement = $offer_arrondissement.'ème' ;
                 $name = $data['name'];
@@ -37,22 +46,28 @@ $pdo = new PDO('mysql:host='.SERVEUR.';dbname='.BASE,NOM,PASSE);
                 $debut = date('d/m/y', strtotime($data['date_start']));
                 $fin = date('d/m/y', strtotime($data['date_end']));
 
-                echo "<a class='offre' href='/message/write/$offer_id/$offer_userid'>";
-                    echo "<div class='flex'>";
-                        echo "<div class='oblique-gauche bg-blanc'>";
-                            echo "<h3 class='noir'>".ucfirst($titre)."</h3>";
-                            echo "<p class='date-lieu saumon'>$debut - $fin - $offer_arrondissement</p>";
-                            echo "<p class='description noir'>$description</p>";
-                        echo "</div>";
-                        echo "<div class='oblique-droite bg-blanc'>";
-                            if ($data['picture'] != 'NULL') {
-                                $picture = base64_encode($data['picture']);
-                                echo "<div class='img-offre' style='background-image: url(data:image/jpg;base64,$picture)''></div>";
-                            }
-                        echo "</div>";
-                    echo "</div>";
-                echo "</a>";
-            }
+                if ($data['picture'] != 'NULL') {
+                    $picture = base64_encode($data['picture']);
+                } else {
+                    $picture = "";
+                }
+                ?>
+                
+                <a class='offre flex bg-blanc' href='<?php echo "/message/write/$offer_id/$offer_userid"; ?>'>
+                        <div class='bloc-offre bloc-offre-text'>
+                            <div id='parallelogram' class='bg-blanc parallelogram-text'>
+                                <p class='noskew'>
+                                    <span class='noir titre-offre'><?php echo $titre; ?></span><br><image class='ico-mini' src='/images/horloge.png' /> <span class='date-lieu saumon'><?php echo $debut." - ".$debut." <image class='ico-mini' src='/images/localisation.png' />  ".$offer_arrondissement; ?></span><br><span class='description noir'><?php echo $description; ?></span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class='bloc-offre bloc-offre-image'>
+                            <div id='parallelogram' class='parallelogram-img'>
+                                <div class='image noskew' style='background-image: url("data:image/jpg;base64,<?php echo $picture; ?>")'></div>
+                            </div>
+                        </div>
+                </a>
+            <?php }
             echo "<h3>Offres dans les autres arrondissements</h3>";
             $req = "SELECT offers.id, offers.user_id, offers.title, offers.description, offers.arrondissement, offers.picture, offers.date_start,  offers.date_end, users.name, users.firstname FROM offers,users WHERE offers.user_id = users.id AND offers.category = '$offer_category' AND offers.status='enabled' AND users.status='enabled' AND offers.arrondissement!=$arrondissement";
 
@@ -62,8 +77,15 @@ $pdo = new PDO('mysql:host='.SERVEUR.';dbname='.BASE,NOM,PASSE);
                 // die(var_dump($data));
                 $offer_id = $data['id'];
                 $offer_userid = $data['user_id'];
-                $titre = $data['title'];
+                $titre = ucfirst($data['title']);
+
                 $description = $data['description'];
+                if (strlen($description) > $max_length)
+                {
+                    $offset = ($max_length - 3) - strlen($description);
+                    $description = substr($description, 0, strrpos($description, ' ', $offset)) . '...';
+                }
+
                 $offer_arrondissement = $data['arrondissement'];
                 $name = $data['name'];
                 $firstname = $data['firstname'];
@@ -71,23 +93,30 @@ $pdo = new PDO('mysql:host='.SERVEUR.';dbname='.BASE,NOM,PASSE);
                 $fin = date('d/m/y', strtotime($data['date_end']));
                 $offer_arrondissement == '1' ? $offer_arrondissement = $offer_arrondissement.'er' : $offer_arrondissement = $offer_arrondissement.'ème' ;
 
-                echo "<a class='offre' href='/message/write/$offer_id/$offer_userid'>";
-                    echo "<div class='flex'>";
-                        echo "<div class='oblique-gauche bg-blanc'>";
-                            echo "<h3 class='noir'>".ucfirst($titre)."</h3>";
-                            echo "<p class='date-lieu saumon'>$debut - $fin - $offer_arrondissement</p>";
-                            echo "<p class='description noir'>$description</p>";
-                        echo "</div>";
-                        echo "<div class='oblique-droite bg-blanc'>";
-                            if ($data['picture'] != 'NULL') {
-                                $picture = base64_encode($data['picture']);
-                                echo "<div class='img-offre' style='background-image: url(data:image/jpg;base64,$picture)''></div>";
-                            }
-                        echo "</div>";
-                    echo "</div>";
-                echo "</a>";
-            }
-        } else {
+                if ($data['picture'] != 'NULL') {
+                    $picture = base64_encode($data['picture']);
+                } else {
+                    $picture = "";
+                }
+
+                ?>
+                
+                <a class='offre flex bg-blanc' href='<?php echo "/message/write/$offer_id/$offer_userid"; ?>'>
+                        <div class='bloc-offre bloc-offre-text'>
+                            <div id='parallelogram' class='bg-blanc parallelogram-text'>
+                                <p class='noskew'>
+                                    <span class='noir titre-offre'><?php echo $titre; ?></span><br><image class='ico-mini' src='/images/horloge.png' /> <span class='date-lieu saumon'><?php echo $debut." - ".$debut." <image class='ico-mini' src='/images/localisation.png' />  ".$offer_arrondissement; ?></span><br><span class='description noir'><?php echo $description; ?></span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class='bloc-offre bloc-offre-image'>
+                            <div id='parallelogram' class='parallelogram-img'>
+                                <div class='image noskew' style='background-image: url("data:image/jpg;base64,<?php echo $picture; ?>")'></div>
+                            </div>
+                        </div>
+                </a>
+            <?php }
+            } else {
             
             $req = "SELECT offers.id, offers.user_id, offers.title, offers.description, offers.arrondissement, offers.picture, offers.date_start,  offers.date_end, users.name, users.firstname FROM offers,users WHERE offers.user_id = users.id AND offers.category = '$offer_category' AND offers.status='enabled' AND users.status='enabled'";
             $statement = $pdo->query($req);
@@ -96,31 +125,46 @@ $pdo = new PDO('mysql:host='.SERVEUR.';dbname='.BASE,NOM,PASSE);
                 
                 $offer_id = $data['id'];
                 $offer_userid = $data['user_id'];
-                $titre = $data['title'];
+                $titre = ucfirst($data['title']);
+                
                 $description = $data['description'];
+                if (strlen($description) > $max_length)
+                {
+                    $offset = ($max_length - 3) - strlen($description);
+                    $description = substr($description, 0, strrpos($description, ' ', $offset)) . '...';
+                }
+
                 $offer_arrondissement = $data['arrondissement'];
                 $name = $data['name'];
                 $firstname = $data['firstname'];
                 $debut = date('d/m/y', strtotime($data['date_start']));
                 $fin = date('d/m/y', strtotime( $data['date_end']));
                 $offer_arrondissement == '1' ? $offer_arrondissement = $offer_arrondissement.'er' : $offer_arrondissement = $offer_arrondissement.'ème' ;
-                
+
                 echo "<a class='offre' href='/message/write/$offer_id/$offer_userid'>";
-                    echo "<div class='flex'>";
-                        echo "<div class='oblique-gauche bg-blanc'>";
-                            echo "<h3 class='noir'>".ucfirst($titre)."</h3>";
-                            echo "<p class='date-lieu saumon'>$debut - $fin - $offer_arrondissement</p>";
-                            echo "<p class='description noir'>$description</p>";
-                        echo "</div>";
-                        echo "<div class='oblique-droite bg-blanc'>";
-                            if ($data['picture'] != 'NULL') {
-                                $picture = base64_encode($data['picture']);
-                            echo "<div class='img-offre' style='background-image: url(data:image/jpg;base64,$picture)''></div>";
-                            }
-                        echo "</div>";
-                    echo "</div>";
-                echo "</a>";
-            }
+                if ($data['picture'] != 'NULL') {
+                    $picture = base64_encode($data['picture']);
+                } else {
+                    $picture = "";
+                }
+
+                ?>
+                
+                <a class='offre flex bg-blanc' href='<?php echo "/message/write/$offer_id/$offer_userid"; ?>'>
+                        <div class='bloc-offre bloc-offre-text'>
+                            <div id='parallelogram' class='bg-blanc parallelogram-text'>
+                                <p class='noskew'>
+                                    <span class='noir titre-offre'><?php echo $titre; ?></span><br><image class='ico-mini' src='/images/horloge.png' /> <span class='date-lieu saumon'><?php echo $debut." - ".$debut." <image class='ico-mini' src='/images/localisation.png' />  ".$offer_arrondissement; ?></span><br><span class='description noir'><?php echo $description; ?></span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class='bloc-offre bloc-offre-image'>
+                            <div id='parallelogram' class='parallelogram-img'>
+                                <div class='image noskew' style='background-image: url("data:image/jpg;base64,<?php echo $picture; ?>")'></div>
+                            </div>
+                        </div>
+                </a>
+            <?php }
         }
         ?>
     </div>
