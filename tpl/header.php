a7bccea7b557c5a3028a01c6770dcd1f0618faed
Juliette Cougnoux <jcougnoux+git@evolix.fr>
2019-05-16 10:49:04 +0200

    $class => $ $couleur

--- tpl/header.php
+++ tpl/header.php
@@ -1,33 +1,33 @@
 <?php 
     if ($_SESSION['user_category']=='admin' || $_SESSION['user_category']=='benevole') {
-        $class = 'vert';
+        $couleur = 'vert';
     }
     elseif ($_SESSION['user_category']=='deloge') 
     {
-        $class = 'saumon';
+        $couleur = 'saumon';
     }
 ?>
 <body class="bg-noir">
     <header>
         <nav class="dr-menu">
             <div class="dr-trigger">
-                <span class="dr-icon dr-icon-menu <?php echo $class ; ?>"></span>
-                <a class="dr-label <?php echo $class ; ?>"><?php echo $_SESSION['user_login']; ?></a>
+                <span class="dr-icon dr-icon-menu <?php echo $couleur ; ?>"></span>
+                <a class="dr-label <?php echo $couleur ; ?>"><?php echo $_SESSION['user_login']; ?></a>
             </div>
             <ul class="flex column center">
-                <li><a class="<?php echo $class ; ?>" href='/accueil'>Accueil</a><br></li>
+                <li><a class="<?php echo $couleur ; ?>" href='/accueil'>Accueil</a><br></li>
                 <?php 
                     if ($_SESSION['user_category']=='admin') { ?>
-                        <li><a class="<?php echo $class ; ?>" href='/admin/register'>Créer un accès</a></li>
-                        <li><a class="<?php echo $class ; ?>" href='/admin/moderation'>Modération accès Délogé</a></li>
+                        <li><a class="<?php echo $couleur ; ?>" href='/admin/register'>Créer un accès</a></li>
+                        <li><a class="<?php echo $couleur ; ?>" href='/admin/moderation'>Modérer un accès</a></li>
                 
                 <?php }
                     if (($_SESSION['user_category']=='admin')||($_SESSION['user_category']=='benevole')) { ?>
-                        <li><a class="<?php echo $class ; ?>" href="/offer/mylist">Mes offres</a></li>
+                        <li><a class="<?php echo $couleur ; ?>" href="/offer/mylist">Mes offres</a></li>
                 <?php } ?>
-                <li><a class="<?php echo $class ; ?>" href="/message/list">Messagerie</a></li>
-                <li><a class="<?php echo $class ; ?>" href="/parametres">Mes paramètres</a></li>
-                <li><a class="<?php echo $class ; ?>" href="/?logout">Déconnexion</a></li>
+                <li><a class="<?php echo $couleur ; ?>" href="/message/list">Messagerie</a></li>
+                <li><a class="<?php echo $couleur ; ?>" href="/parametres">Mes paramètres</a></li>
+                <li><a class="<?php echo $couleur ; ?>" href="/?logout">Déconnexion</a></li>
             </ul>
         </nav>
     </header>
