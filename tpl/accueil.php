8d2170b3eb96f875fd060015c61e9b33014829a1
Juliette Cougnoux <jcougnoux+git@evolix.fr>
2019-05-16 18:41:13 +0200

    WIP suite

--- tpl/accueil.php
+++ tpl/accueil.php
@@ -5,6 +5,16 @@
 <a href="/offer/mylist">
     <div class="header2 bg-blanc flex center">
         <h2 class="saumon">
+            <?php 
+                if ($_SESSION['user_category']=='admin' || $_SESSION['user_category']=='benevole') 
+                {
+                    echo 'Mes offres';
+                }
+                else
+                {
+                    echo 'Offres disponibles';
+                }
+            ?>
         </h2>
         <img class="fleche-droite" src="../images/fleche-droite.png" alt="">
     </div>
@@ -30,27 +40,27 @@ if (($_SESSION['user_category']=='admin')||($_SESSION['user_category']=='deloge'
     <h2 class="blanc">Consulter les offres</h2>
     <section class="tableau-de-bord flex center wrap">
         <a class="flex center column" href='/offer/list/restauration'>
-            <img class="icone" src="./images/restauration-<?php echo $couleur ; ?>.png" alt="">
+            <img class="icone" src="./images/restauration.png" alt="">
             <h3>Restauration</h3>
         </a>
         <a class="flex center column" href='/offer/list/blanchisserie'>
-            <img class="icone" src="./images/blanchisserie-<?php echo $couleur ; ?>.png" alt="">
+            <img class="icone" src="./images/blanchisserie.png" alt="">
             <h3>Blanchisserie</h3>
         </a>
         <a class="flex center column" href='/offer/list/mobilite'>
-            <img class="icone" src="./images/mobilite-<?php echo $couleur ; ?>.png" alt="">
+            <img class="icone" src="./images/mobilite.png" alt="">
             <h3>Mobilté</h3>
         </a>
         <a class="flex center column" href='/offer/list/loisir'>
-            <img class="icone" src="./images/loisir-<?php echo $couleur ; ?>.png" alt="">
+            <img class="icone" src="./images/loisirs.png" alt="">
             <h3>Loisir</h3>
         </a>
         <a class="flex center column" href='/offer/list/don'>
-            <img class="icone" src="./images/dons-<?php echo $couleur ; ?>.png" alt="">
+            <img class="icone" src="./images/dons.png" alt="">
             <h3>Dons</h3>
         </a>
         <a class="flex center column" href='/offer/list/autre'>
-            <img class="icone" src="./images/autre-<?php echo $couleur ; ?>.png" alt="">
+            <img class="icone" src="./images/autre.png" alt="">
             <h3>Autre</h3>
         </a>
     </section>
@@ -76,7 +86,7 @@ if (($_SESSION['user_category']=='admin')||($_SESSION['user_category']=='deloge'
                 <h3>Mobilté</h3>
             </a>
             <a class="flex center column" href='/offer/new/loisir'>
-                <img class="icone" src="./images/loisir-ajout.png" alt="">
+                <img class="icone" src="./images/loisirs-ajout.png" alt="">
                 <h3>Loisir</h3>
             </a>
             <a class="flex center column" href='/offer/new/don'>
