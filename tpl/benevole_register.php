4c940a1db3cafebd4d856e5bff64f97970e65f19
Juliette Cougnoux <jcougnoux+git@evolix.fr>
2019-05-20 15:01:46 +0200

    test style iphone

--- tpl/benevole_register.php
+++ tpl/benevole_register.php
@@ -108,13 +108,13 @@ L'equipe Aouf
                     <label for="nonbinaire">Non binaire</label>
             </section>
 
-            <div class="">
+            <div class="flex">
                 <input type="checkbox" name="cgu" value=""><label>J'ai lu et j'accepte les <a class="small-text saumon" href="/cgu">CGU</a></label>
             </div>
-            <div class="">
+            <div class="flex">
                 <input type="checkbox" name="myInfos" value=""><label>J'accepte que les informations saisies soient utilisées pour la gestion de l'application <strong>Aouf</strong></label>
             </div>
-            <div class="">
+            <div class="flex">
                 <input type="checkbox" name="aoufInfo" value=""><label>J'accepte de recevoir des informations d'Aouf <span class="saumon">(optionnel)</span></label>
             </div>
 
