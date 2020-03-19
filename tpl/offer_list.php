1481256b67e4e878c70bfa20c61d448c8ef2c9ea
Gregory Colpart <gcolpart+git@evolix.fr>
2020-03-19 21:33:36 +0100

    limitation date pour announces

--- tpl/offer_list.php
+++ tpl/offer_list.php
@@ -24,7 +24,7 @@ $max_length = 60;
         if (($arrondissement != '')&&($arrondissement != '0')) {
 
             echo "<h3>Offres dans votre arrondissement ($arrondissement_beautify)</h3>";
-            $req = "SELECT offers.id, offers.user_id, offers.title, offers.description, offers.arrondissement, offers.picture, offers.date_start,  offers.date_end, users.name, users.firstname FROM offers,users WHERE offers.user_id = users.id AND offers.category = '$offer_category' AND offers.status='enabled' AND users.status='enabled' AND offers.arrondissement=$arrondissement";
+            $req = "SELECT offers.id, offers.user_id, offers.title, offers.description, offers.arrondissement, offers.picture, offers.date_start,  offers.date_end, users.name, users.firstname FROM offers,users WHERE offers.user_id = users.id AND offers.category = '$offer_category' AND offers.status='enabled' AND users.status='enabled' AND offers.arrondissement=$arrondissement AND date_start < NOW() and date_end > NOW()";
             $statement = $pdo->query($req);
             
             while ($data = $statement->fetch()) {
