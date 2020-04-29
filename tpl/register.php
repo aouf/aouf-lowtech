f964d497c952f18d5b93009f99a7ce49335759e6
Juliette Cougnoux <jcougnoux+git@evolix.fr>
2020-04-29 17:59:30 +0200

    Ajout des textes d'explication des différents profils + coordinateur/MCS

--- tpl/register.php
+++ tpl/register.php
@@ -11,12 +11,15 @@ require_once 'head.php';
                 <div class="formulaires flex column center">
                     <form class="flex column center" action="/benevole/register" method="post">
                         <button class='bg-vert' type='submit' value='Connexion'>Je veux aider</button>
+                        <center>Je veux proposer mon aide, répondre a un besoin</center>
                     </form>
                     <form class="flex column center" action="/coordinateur/register" method="post">
-                        <button class='bg-vert' type='submit' value='Connexion'>Je veux coordonner de l'aide</button>
+                        <button class='bg-saumon noir' type='submit' value='Connexion'>Je veux coordonner de l'aide/MCS</button>
+                        <center>Collectif, asso, travailleur social… je souhaite commander couches/lait pour des familles ou poster un besoin</center>
                     </form>
                     <form class="flex column center" action="/deloge/register" method="post">
                         <button class='bg-saumon noir' type="submit"value='Connexion'>J'ai besoin d'aide</button>
+                        <center>J'ai un besoin d'urgence</center>
                     </form>
                     <center class="register-small-links"><a class="small-text under vert" href='/'>Retour</a></center>
                 </div>
